345977906dc10831ec97e2fdde655a10d620e363
addakridler <addakridler@gmail.com>
2021-08-04 11:01:34 -0400

    functional submit question button question url no longer 'not null' for sql
    Co-authored-by:
    Rachel Patterson <rjpatt@users.noreply.github.com>
    Co-authored-by: yarnett <yarnett@users.noreply.github.com>

--- client/containers/NewMainAppContainer.jsx
+++ client/containers/NewMainAppContainer.jsx
@@ -4,27 +4,27 @@ import QuestionsContainer from "./QuestionsContainer";
 import CreateQuestionForm from "../components/CreateQuestionForm";
 import Chat from "../components/Chat";
 
-const newMainAppContainer = () =>{
+// const newMainAppContainer = () =>{
 
-  const[fetchData, setFetchData] = useState(false);
-  const[questions, setQuestions] = useQuestions([]);
+//   const[fetchData, setFetchData] = useState(false);
+//   const[questions, setQuestions] = useQuestions([]);
 
-  useEffect(() => {
-    fetch("/api/questions")
-    .then((res) => res.json())
-    .then((questionData) => {
-      if (!Array.isArray(questionData)) questionData = [];
-      setFetchData(true);
-      setQuestions(questionData);
-    })
-    .catch((err) =>
-      console.log("error in NewMainAppContainer fetch: ", err)
-    );
-  })
+//   useEffect(() => {
+//     fetch("/api/questions")
+//     .then((res) => res.json())
+//     .then((questionData) => {
+//       if (!Array.isArray(questionData)) questionData = [];
+//       setFetchData(true);
+//       setQuestions(questionData);
+//     })
+//     .catch((err) =>
+//       console.log("error in NewMainAppContainer fetch: ", err)
+//     );
+//   })
 
-}
-
-
+// }
 
 
-export default NewMainAppContainer;
+
+
+// export default NewMainAppContainer;
