716d2fdbdcac0270b37aa4c2450cf50850d93b21
addakridler <addakridler@gmail.com>
2021-08-04 17:18:20 -0400

    front end structure set working submit new questions/answers re-rendering of previous questions/answers on submit

--- client/components/QuestionCard.jsx
+++ client/components/QuestionCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 // import { FontAwesomeIcon as FAIcon } from '@fortawesome/react-fontawesome';
 // import { faQuestionCircle } from '@fortawesome/free-regular-svg-icons';
 import Answer from './Answer';
@@ -11,30 +12,27 @@ import { Link } from 'react-router-dom';
 const QuestionCard = (props) => {
   //props from QuestionsContainer get assigned to each instance of QuestionCard
   const { id, title, description, creator, isOpen } = props;
-  const[renderInputBox, setRenderInputBox] = useState(false)
-
-  // console.log(props);
-
-// let answerButton = '';
-
-//   if (isActive===true){
-//    answerButton = '<Button variant="primary">Answer this question</Button>';
-//   }
-  const handleSubmit 
+  // const [state, setState] = useState({
+  //   id: id, 
+  //   title: title, 
+  //   description: description
+  // });
 
 
   return (
-
-
-<div key={id} className="question-container">
-  <h3>{title}</h3>
-  <p>{description}</p>
-  <button variant="primary" className="min-button" onClick={e => setRenderInputBox=true}>Answer question</button>
-  { renderInputBox ? <Answer setRenderInputBox={setRenderInputBox} /> : null };
-</div>
-    
-
+    <div key={id} className="question-container">
+      <h3>{title}</h3>
+      <p>{description}</p>
+      <Link to={{
+        pathname: `/question/${id}`,
+        state: {
+          id: id, 
+          title: title, 
+          description: description
+        }
+     }}><button variant="primary" className="min-button">Answer question</button></Link>
+    </div>  
   );
 };
 
-export default QuestionCard;
+export default QuestionCard; 
